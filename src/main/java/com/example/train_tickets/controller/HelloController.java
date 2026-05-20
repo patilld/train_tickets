90ca53a37992f0b86cb1b23d2801c09ae4a5c6d8
Lalit Patil <patilld94@gmail.com>
2026-05-20 08:43:04 +0530

    added HelloController

---
+++ src/main/java/com/example/train_tickets/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.train_tickets.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+
+        return "Hello World!";
+    }
+}
