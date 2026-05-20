812a305c669ed45cf9e6429de8b1c320643be751
Lalit Patil <patilld94@gmail.com>
2026-05-20 08:25:59 +0530

    added all files

---
+++ src/main/java/com/example/train_tickets/TrainTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.train_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/train_tickets/TrainTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.train_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
